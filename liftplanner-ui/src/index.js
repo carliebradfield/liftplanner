f0607a78df3658c7d14584afda531e2542f46ac6
Carlie Bradfield <20054615+carliebradfield@users.noreply.github.com>
2021-11-28 22:13:53 +0000

    Resolved cors policy issue with manifest.json; moved bootstrap stylesheet link to index.html

--- liftplanner-ui/src/index.js
+++ liftplanner-ui/src/index.js
@@ -8,13 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   
-    <React.StrictMode>
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-        crossorigin="anonymous"
-      />    
+    <React.StrictMode>      
       <BrowserRouter>
         <App />
       </BrowserRouter>
